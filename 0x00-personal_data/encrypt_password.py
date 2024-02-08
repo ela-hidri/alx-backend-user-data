2d8def2b4085b71be0a1a284676c364b22046df6
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-08 22:12:18 +0100

    adding encrypt_pass

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,6 +2,7 @@
 """Encrypting passwords"""
 import bcrypt
 
+
 def hash_password(password: str) -> bytes[str]:
     """ hash password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
