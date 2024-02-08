6f6b5137868f496c4b310a8ec7a7655f91b1f3c7
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-08 22:34:43 +0100

    adding is_valid

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """Encrypting passwords"""
 import bcrypt
-from typing import ByteString
 
 
-def hash_password(password: str) -> ByteString:
+def hash_password(password: str) -> bytes:
     """ hash password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """valid password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
