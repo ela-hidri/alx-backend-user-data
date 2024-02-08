f2f7e0e5bd803946f21d548557e29b1a619634ee
ela-hidri <elahidri.eh@gmail.com>
2024-02-08 21:19:20 +0000

    fixing encrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Encrypting passwords"""
 import bcrypt
+from typing import ByteString
 
 
-def hash_password(password: str) -> bytes[str]:
+def hash_password(password: str) -> ByteString:
     """ hash password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
