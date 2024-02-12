f9a4912b93263aa82f3676327d00194ddfd1cf8e
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-12 20:09:40 +0100

    changing require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,14 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ required auth
         """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        if path[-1] != "/" and f'{path}/' in excluded_paths:
+            return False 
+        return True
+        
 
     def authorization_header(self, request=None) -> str:
         """ def Authorization header
