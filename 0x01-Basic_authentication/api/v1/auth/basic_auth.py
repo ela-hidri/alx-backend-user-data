bb0bf3d637e20ee8187b882d4ca462d25d2d3f93
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-12 21:32:26 +0100

    changing Basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -20,3 +20,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """returns the decoded value of a Base64 string 
+        base64_authorization_header"""
+        if base64_authorization_header is None:
+            return None
+        if isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+        except:
+            return None
+        return decoded.decode('utf-8')
