46107a1008885d0dce6604598b5eead891ad5980
ela-hidri <elahidri.eh@gmail.com>
2024-02-12 19:53:07 +0000

    adding changes

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,9 +7,10 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+import logging
 
 
-
+logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -23,12 +24,12 @@ if os.getenv('AUTH_TYPE') == 'auth':
 def before_request_func():
     """  filtering of each request"""
     if auth is None:
-        pass
+        return
     routes = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if not auth.require_auth(request.path, routes):
-        pass
+        return
     if auth.authorization_header(request) is None:
-        abort(4.1)
+        abort(401)
     if auth.current_user(request) is None:
         abort(403)
 
