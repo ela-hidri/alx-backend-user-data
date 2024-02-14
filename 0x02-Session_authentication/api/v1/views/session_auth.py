525bca1fce502bbaa7745b249eefc9c9c3f99f93
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-14 22:01:42 +0100

    adding logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -18,19 +18,27 @@ def login():
     email = request.form.get('email')
     password = request.form.get('password')
     if email is None or email == "":
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     if password is None or password == "":
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
     rst = User().search({"email": email})
     if len(rst) == 0:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     user: User = rst[0]
     if not user.is_valid_password(password):
-        return jsonify({ "error": "wrong password" }),401
+        return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
     sessin_id = auth.create_session(user.id)
     logger.info(sessin_id)
     out = jsonify(user.to_json())
     out.set_cookie(os.getenv('SESSION_NAME'), sessin_id)
     return out
-    
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def Logout():
+    """ logout """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
