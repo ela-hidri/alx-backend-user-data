edfefaca397c2e997ea665753ab7be79e000929a
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-14 19:15:59 +0100

    fixing file

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,9 +12,9 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """creates a Session ID for a user_id"""
         if user_id is None:
-            None
+            return None
         if not isinstance(user_id, str):
-            None
+            return None
         SessionId = str(uuid.uuid4())
         self.user_id_by_session_id[SessionId] = user_id
         return SessionId
