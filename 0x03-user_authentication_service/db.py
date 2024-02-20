c344eb22be9fb08f525de75455442ace2540d807
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-20 21:41:25 +0100

    remove import

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,7 +3,6 @@
 """
 from typing import Dict
 from sqlalchemy import create_engine
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
