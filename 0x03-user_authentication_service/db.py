aadde7b0d1deb4d823eb7af9f09f7bde74c2e514
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-20 21:40:22 +0100

    changes

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -39,15 +39,3 @@ class DB:
         session.add(user)
         session.commit()
         return user
-
-    def find_user_by(self, **kwargs: Dict) -> User:
-        """finds user"""
-        query = self._session.query(User)
-        try:
-            query = query.filter_by(**kwargs)
-        except InvalidRequestError:
-            raise InvalidRequestError
-        result = query.first()
-        if result is None:
-            raise NoResultFound
-        return result
