562dd9699432ade7730d816db47656b54320055e
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-20 21:42:49 +0100

    changes

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,6 +3,7 @@
 """
 from typing import Dict
 from sqlalchemy import create_engine
+from sqlalchemy.orm.exc import InvalidRequestError, NoResultFound
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
