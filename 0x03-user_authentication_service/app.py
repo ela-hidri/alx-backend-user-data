e2275c1968b07314301a91ad3f3e5c56b77307e9
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-20 23:05:36 +0100

    changing app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 """app module
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask("__name__")
+AUTH = Auth()
 
 
 @app.route('/')
 def hello():
     """ return message"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """register user if not exist """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": "<registered email>", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
 
 
 if __name__ == "__main__":
