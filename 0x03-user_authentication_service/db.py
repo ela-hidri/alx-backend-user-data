4c67691d27d84ef2206b4b4d0a31a39b17bc1442
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-20 20:32:07 +0100

    fixing

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ save user to database"""
         user = User(email=email, hashed_password=hashed_password)
         session = self._session
