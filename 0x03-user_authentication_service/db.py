d62afa5e196ba1a340e86428c123b6bffddc7c70
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-20 21:30:18 +0100

    fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from typing import TypeVar
+
 from user import Base, User
 
 
