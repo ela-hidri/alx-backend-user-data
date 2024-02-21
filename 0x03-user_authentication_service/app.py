9abcfdafb758f4dc3081b501421879cab147e441
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-21 21:45:27 +0100

    fixing style

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -86,7 +86,7 @@ def update_password():
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
-    try: 
+    try:
         AUTH.update_password(reset_token, new_password)
     except Exception:
         abort(403)
