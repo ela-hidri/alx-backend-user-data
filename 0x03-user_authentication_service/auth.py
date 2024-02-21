be55defd7119e7213a24f81079f125023e834f1b
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-21 21:31:09 +0100

    fixing changes

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,9 +86,9 @@ class Auth:
         """ reset password """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed_pass = _hash_password(password)
-            self._db.update_user(user_id=user.id, password=hashed_pass)
-            self._db.update_user(user_id=user.id, reset_token=None)
         except Exception:
             raise ValueError
+        hashed_pass = _hash_password(password)
+        self._db.update_user(user_id=user.id, hashed_password=hashed_pass)
+        self._db.update_user(user_id=user.id, reset_token=None)
         
