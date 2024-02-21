8a7218c3ac8cdc2141ce3604f1cef859dd2519f8
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-21 21:42:10 +0100

    fixing

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,7 +80,7 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
-@app.route('/reset_password', methods=['PUT'])
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     """ update password """
     email = request.form.get("email")
