32794d6bcc4ba180ce38d96157fb1670b7358077
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-21 21:28:52 +0100

    adding changes

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,3 +81,14 @@ class Auth:
         except (NoResultFound, InvalidRequestError, ValueError):
             raise ValueError
         return uuid
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ reset password """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_pass = _hash_password(password)
+            self._db.update_user(user_id=user.id, password=hashed_pass)
+            self._db.update_user(user_id=user.id, reset_token=None)
+        except Exception:
+            raise ValueError
+        
