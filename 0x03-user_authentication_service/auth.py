250d409d029953bf367a58fe43702e67b898c855
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-21 20:22:33 +0100

    fixing changes

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,7 +66,7 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """ destroys session"""
         try:
-            user = self._db.find_user_by(user_id=user_id)
+            user = self._db.find_user_by(id=user_id)
             self._db.update_user(user_id=user.id, session_id=None)
         except (NoResultFound, InvalidRequestError, ValueError):
             return None
