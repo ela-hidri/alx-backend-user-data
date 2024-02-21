d011160d47d40a457aa8c44ecfa7f7ae9a4ddae5
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-21 20:40:42 +0100

    adding changes

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -41,7 +41,7 @@ def login():
         abort(401)
 
 
-@app.route('/sessions', methods=['DELETE'])
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
     """destroy the session and redirect the user to/"""
     session_id = request.cookies.get("session_id")
@@ -51,6 +51,17 @@ def logout():
     else:
         AUTH.destroy_session(session_id)
         return redirect("/")
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def method_name():
+    """ find user by session"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    else:
+        return jsonify({"email": user.email})
 
 
 if __name__ == "__main__":
