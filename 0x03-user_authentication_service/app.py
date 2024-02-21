8ea1bdff0c8400ecdcc9a6606a93038a4a8a0370
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-21 21:41:38 +0100

    fixing

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -90,7 +90,7 @@ def update_password():
         AUTH.update_password(reset_token, new_password)
     except Exception:
         abort(403)
-    return jsonify({"email": email, "message": "Password updated"})
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 
